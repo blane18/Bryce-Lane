3889997d7bd97b90e0f079ef87fce9689c6ea452
Bryce Lane <60410721+blane18@users.noreply.github.com>
2020-02-12 15:37:28 -0600

    Add files via upload

--- useIt.java
+++ useIt.java
@@ -1,50 +1,62 @@
 class Xamples{
-	
+
+
 	public String ShowYours() {
-		
+		System.out.println("I will show mine");
 	}
 	
 	public String ShowMine() {
-		
+		System.out.println("you show yours");
 	}
-	
+
+	/*
 	public int ShowYours(String C) {
-		
+		System.out.println("how many cards do you have");
 	}
 	
 	public int ShowMine(String D) {
-		
+		System.out.println("I have 5 cards");
 	}
 	
 	public double ShowYours(int C) {
-		
+		System.out.print(6);
 	}
 	
 	public double ShowMine(int D) {
-		
+		System.out.print(5);
 	}
-
+*/
 }
-class useIt{
+class useIt {
+	public static void main(String[] args){
 	
 	Xamples iWill = new Xamples();
 	
-	int saveInt = 0;
-	int SaveString = "testing";
-	int SaveDouble = 42.42;
+	 
+	
 	
 	iWill.ShowMine();
-	
+		//System.out.print(ShowMine);
+		
 	iWill.ShowYours();
-	
+		//System.out.println(ShowYours);
+	/*
 	iWill.ShowMine("i'm telling mom");
+		System.out.println(ShowMine);
 	
 	iWill.ShowYours("ok");
+		System.out.println(ShowYours);
 	
 	iWill.ShowMine(42);
-	
+		System.out.println(ShowMine);
+		
 	iWill.ShowYours(24);
+		System.out.println(ShowYours);*/
 	
-	System.out.println();
-
+	
+	
+	
+	
+	
+	}
 }
